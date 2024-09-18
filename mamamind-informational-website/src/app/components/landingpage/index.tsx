7cb023106df41ecba1637a0ffd5a035513406f98
Marthamwongeli <marthamutuaa@gmail.com>
2024-09-18 12:20:06 +0300

    added landingpage

---
+++ mamamind-informational-website/src/app/components/landingpage/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col md:flex-col lg:flex-row px-6 lg:px-12 py-6 items-center justify-between ipad:flex-col">
+      
+      <div className="mb-8 lg:order-last px-6 w-1/2">
+        <img
+          src="landingPageImage.png"
+          alt="Mother and child"
+          className=" md:w-[400px] md:h-[400px] lg:w-[517px] lg:h-[758px] object-contain lg:object-cover mx-auto nest-hub:w-[400px] nest-hub:h-[400px] nest-hub-max:w-[600px] nest-hub-max:h-[600px]"
+        />
+      </div>
+      
+      <div className="w-full text-center  lg:text-left lg:ml-8 lg:pt-20">
+        <h1 className="text-[28px] md:text-[55px] lg:text-[66px] mb-8 font-bold text-orange-600 font-nunito nest-hub:text-size-26 text-left">
+          Thrive with MamaMind
+        </h1>
+        <p className="text-[25px] md:text-[44px] lg:text-[37px] font-nunito mb-8 text-gray-800 nest-hub:text-size-24 text-left nest-hub-max:text-size-24">
+          Immerse yourself in a supportive community and <br />follow a care guide crafted for your postpartum journey.
+        </p>
+
+        <div className="flex flex-col lg:flex-row gap-4 lg:gap-10 pt-4">
+          <button className="px-4 py-2 lg:px-6 lg:py-3 text-[20px] md:text-[28px] lg:text-[22px] text-white bg-Blue font-nunito hover:bg-blue-700 rounded-lg ">
+            Get a Demo
+          </button>
+          <button className="px-4 py-2 lg:px-6 lg:py-3 text-[20px] md:text-[28px] lg:text-[22px] w-full lg:w-64 border-2 border-Blue font-nunito text-Blue hover:bg-gray-300 rounded-lg ">
+            Get the Dashboard
+          </button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Home;
--- mamamind-informational-website/tailwind.config.ts
+++ mamamind-informational-website/tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +12,45 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      fontSize: {
+        '37px': '37px',
+        '66px': '66px',
+        '27px': '27px',
+        "size-24": "24px",
+        "size-18": "18px",
+      },
+      colors: {
+        Orange: '#F18721',
+        Blue: '#02A6A6',
+        Black:'#000000'
+      },
+      width: {
+        '517px': '517px', 
+      },
+      screens: {
+        ipad: {
+          raw: "(min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait)",
+        },
+        "nest-hub": {
+          raw: "(min-width: 1024px) and (max-width: 1024px) and (min-height: 600px) and (max-height: 600px)",
+        },
+        "nest-hub-max": {
+          raw: "(min-width: 1280px) and (max-width: 1280px) and (min-height: 800px) and (max-height: 800px)",
+        },
+        "galaxy-fold": {
+          raw: "(min-width: 280px) and (max-width: 280px) and (min-height: 653px) and (max-height: 653px)",
+        },
+      },
+      height: {
+        '758px': '758px',
+      },
     },
   },
   plugins: [],
 };
 export default config;
+
+
